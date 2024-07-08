b2413fcbf1bc36aed7dc5881667e2e4bfce41ea1
mireya111 <mireya.garcia@epn.edu.ec>
2024-07-07 23:31:51 -0500

    version01_García Mireya

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Inserción de datos en mysql");
+        frame.setContentPane(new Insertar().insertaDatos);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(500,400);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
