ff7e59447b7ece2ded3d6ab5247ae0f175cc4fb7
mireya111 <mireya.garcia@epn.edu.ec>
2024-07-07 23:33:32 -0500

    version02_García Mireya

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /*Visualización de la ventana emergente*/
         JFrame frame = new JFrame("Inserción de datos en mysql");
         frame.setContentPane(new Insertar().insertaDatos);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
